7d886494aeb7e48ad921d5801de9272952d2097e
SofieKoefoed <sofiekoefoed@gmail.com>
2016-02-21 20:18:39 +0100

    Programming assignment week 3
    This is my take on trying to solve the problems given in this weeks
    assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions should do what is needed to be done in the assignment 
+## Caching the Inverse of a Matrix.
 
-## Write a short comment describing this function
+## The point of this function is to create a special "matrix" object 
+## that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(get = get,
+       setsolve = setsolve,
+       getsolve = getsolve
+  )
 }
 
-
-## Write a short comment describing this function
+## This function should compute the inverse of the matrix returned by the funktion
+## makeCacheMatrix above. If the inverse has been calculated before and there is no 
+## change in the matrix, then the cachesolve funktion should retrieve 
+## the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  blob <- x$get()
+  s <- solve(blob, ...)
+  x$setsolve(s)
+  s
 }
